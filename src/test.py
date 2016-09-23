3fbe7de5a2ec403515d9f1fa93487de072de3ec9
Paul <pavel.tiron@gmail.com>
2016-09-23 12:38:12 -0700

    updates

--- src/test.py
+++ src/test.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import cv
-
+import random
+import string
 
 if __name__ == "__main__":
     capture = cv.CaptureFromCAM(1)
     frame = cv.QueryFrame(capture)
-    cv.SaveImage("media/capture.jpg", frame)
+    random_part = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(11))
+    cv.SaveImage("media/capture_%s.jpg" % random_part, frame)
