8864287302151a672d3fa315f8fc3827a1eeb3c4
Paul <pavel.tiron@gmail.com>
2019-01-25 17:14:00 -0800

    added script for installation opencv2 ubuntu

---
+++ install_cv2_.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install python-opencv -y
+
+echo "Congratulations! You have just installed OpenCV 2. And that's all, folks! :P"
